2479605582a9d5ecf765a50b98558d2f386ab235
Calena <agaru.corp@gmail.com>
2025-07-12 22:53:12 -0400

    fixes hero

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -42,8 +42,8 @@ const About = ({ language }: AboutProps) => {
           
           <div className="relative">
             <img 
-              src="https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
-              alt="Yerba Mate and Tea Leaves"
+              src="/nuestra-historia.jpg"
+              alt="Historia Cannlabs"
               className="rounded-2xl shadow-2xl"
             />
           </div>
